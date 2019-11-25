ab3ded5299316eaf66d24477cbed9ebeef0bc5d2
tlbcc <tlbcc@foxmail.com>
2019-11-25 16:54:33 +0800

    完成挖金矿问题

--- src/pers/tlbcc/algorithm/dp/DigGoldMine.java
+++ src/pers/tlbcc/algorithm/dp/DigGoldMine.java
@@ -15,17 +15,30 @@ public class DigGoldMine {
      * @return
      */
     public static int count(int[][] n, int w) {
-        int[][] p = new int[n.length + 1][w];
-        int max = 0;
+        int[][] p = new int[n.length + 1][w + 1];
         for (int i = 1; i <= n.length; i++) {
-            for (int j = 0; j < w; j++) {
-                p[i][j] = Math.max(p[i - 1][j], p[i - 1][j - n[i - 1][1] + n[i - 1][0]]);
-                max = Math.max(max, p[i][j]);
+            for (int j = 1; j <= w; j++) {
+                if (j < n[i - 1][1]) {
+                    p[i][j] = p[i - 1][j];
+                } else {
+                    p[i][j] = Math.max(p[i - 1][j], p[i - 1][j - n[i - 1][1]] + n[i - 1][0]);
+                }
+                print(p);
             }
         }
-        return max;
+        return p[n.length][w];
     }
 
+    private static void print(int[][] p) {
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < p[i].length; j++) {
+                System.out.print(p[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("=================================== ");
+    }
+    
     public static void main(String[] args) {
         int[][] n = new int[][]{{400, 5}, {500, 5}, {200, 3}, {300, 4}, {350, 3}};
         System.out.println(count(n, 10));
